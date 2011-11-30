1fc20cf0785bfe50fdbd1b6a906c47d3c33e7f45
Nick Reid <nickreid@nickreid.com>
2011-11-29 19:07:14 -0800

    added variablilty to data_treemap function

--- charts.py
+++ charts.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, request, render_template, jsonify
 from models import *
 
 app = Flask(__name__)
@@ -21,27 +21,63 @@ def data():
 			results[test.ethnicity]['amount'] += 1
 	return jsonify(results)
 	
-@app.route('/data/treemap.json')
+@app.route('/data/treemap.json',methods=['GET','POST'])
 def data_treemap():
 	results = {}
+	axis = 'ethnicity'
+	if request.method == 'POST' and 'axis' in request.post:
+		axis = request.post['axis']
 	for test in Test.query.all():
-		if test.ethnicity not in results:
-			results[test.ethnicity] = {'size':1,'Methadone':0,'Community Based Organization':0,'Male':0,'Female':0,'Trans':0}
+		key = ""
+		if axis == 'ethnicity':
+			key = test.ethnicity
+		if axis == 'gender':
+			key = test.gender
+		if key == "":
+			return False
+		
+		if key not in results:
+			results[key] = {'size':1}
 		else:
-			results[test.ethnicity]['size'] += 1
-		if test.siteID == 'Methadone':
-			results[test.ethnicity]['Methadone'] += 1
-		else:
-			results[test.ethnicity]['Community Based Organization'] += 1
-		if test.gender == 'MALE':
-			results[test.ethnicity]['Male'] += 1
-		elif test.gender == 'FEMALE':
-			results[test.ethnicity]['Female'] += 1
-		else:
-			results[test.ethnicity]['Trans'] += 1
+			results[key]['size'] += 1
+		
+		if axis != 'siteID':
+			if test.siteID == 'Methadone':
+				results[key] = add_or_increment('Methadone',results[key])
+			else:
+				results[key] = add_or_increment('Community Based Organization',results[key])
+		if axis != 'gender':
+			if test.gender == 'MALE':
+				results[key] = add_or_increment('Male',results[key])
+			elif test.gender == 'FEMALE':
+				results[key] = add_or_increment('Female',results[key])
+			else:
+				results[key] = add_or_increment('Trans',results[key])
+		if axis != 'ethniciy':
+			if test.ethnicity == 'WHITE':
+				results[key] = add_or_increment('White',results[key])
+			if test.ethnicity == 'AFRICAN AMERICAN':
+				results[key] = add_or_increment('African American',results[key])
+			if test.ethnicity == 'HISPANIC/LATINO':
+				results[key] = add_or_increment('Latino',results[key])
+			if test.ethnicity == 'ASIAN':
+				results[key] = add_or_increment('Asian',results[key])
+			if test.ethnicity == 'OTHER':
+				results[key] = add_or_increment('Other',results[key])
+			if test.ethnicity == 'DECLINED':
+				results[key] = add_or_increment('Declined',results[key])
+			if test.ethnicity == 'NATIVE AMERICAN':
+				results[key] = add_or_increment('Native American',results[key])
 		
 	ordered_results = []
 	for result in results:
 		results[result]['name'] = result
 		ordered_results.append(results[result])
 	return jsonify({'name':'tests','children':ordered_results})
+	
+def add_or_increment(key,arr):
+	if key not in arr:
+		arr[key] = 1
+	else:
+		arr[key] += 1
+	return arr
