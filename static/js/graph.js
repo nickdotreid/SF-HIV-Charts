39b8fa68dcf7e0566f8ba6cd244659bf7ff6f7ab
Nick Reid <nickreid@nickreid.com>
2011-11-30 18:28:56 -0800

    switched about population graph to use radio button code

--- static/js/graph.js
+++ static/js/graph.js
@@ -8,7 +8,7 @@ jQuery(document).ready(function(){
 		}
 		population = data['Population'];
 		for(demographic in population){
-			$(chart).append('<div class="bar" data-demographic="'+demographic+'"><h3>'+demographic+'</h3><div class="total"><div class="percent number"></div> of <div class="number"></div> People</div><div class="container"><div class="line population"><div class="percent"><div class="number"></div>%</div></div><div class="line incidence"><div class="percent"><div class="number"></div>%</div></div></div></div>');
+			$(chart).append('<div class="bar" data-demographic="'+demographic+'"><h3>'+demographic+'</h3><div class="total"><div class="percent number"></div> of <div class="number"></div> People</div><div class="container"><div class="line"><div class="percent"><div class="number"></div>%</div></div></div></div>');
 		}
 		chart.trigger("draw");
 	}).bind("draw",function(event){
@@ -17,103 +17,36 @@ jQuery(document).ready(function(){
 		if(!data){
 			return false;
 		}
-		comp_total = $(".control.selected").data("value");
-		total = false;
-		switch(comp_total){
-			case 'incidence':
-				total = 'total_pos';
-				$(".description .numerator").html("New HIV Cases");
-				$(".description .demonator").html("HIV+ Population by Demographic");
-				break;
-			case 'incidence-total':
-				total = data['Population']['Total'];
-				$(".description .numerator").html("New HIV Cases");
-				$(".description .demonator").html("HIV+ Population Total");
-				break;
-			case 'demographic':
-				total = 'own';
-				$(".description .numerator").html("HIV+ Population");
-				$(".description .demonator").html("SF Population by Demographic");
-				break;
-			case 'city':
-				total = data['Census']['Total'];
-				$(".description .numerator").html("HIV+ Population");
-				$(".description .demonator").html("SF Population Total");
-				break;
-		}
-		if(!total){
-			alert("no draw type");
-			return false;
-		}
 		max_percent = false;
 		$('.bar',chart).each(function(){
 			bar = $(this);
-			local_total = total
-			if(total == 'own'){
-				local_total = data['Census'][bar.data("demographic")];
+			local_total = 1;
+			if($("#demographic").attr("checked")){
+				local_total = data[$(".controls .denominator input:checked").val()][bar.data("demographic")];
+			}else{
+				local_total = data[$(".controls .denominator input:checked").val()]['Total'];
 			}
-			if(total == 'total_pos'){
-				local_total = data['Population'][bar.data("demographic")];
-			}
+			percent = data[$(".controls .numerator input:checked").val()][bar.data("demographic")]/local_total;
 			$(".total .number",bar).html(addCommas(local_total));
-			bar.data("population-percent",data['Population'][bar.data("demographic")]/local_total);
-			bar.data("incidence-percent",data['Incidence'][bar.data("demographic")]/local_total);
+			bar.data("percent",percent);
 			bar.data("total",local_total);
 			
-			$(".population .number",bar).html(formatPercent(bar.data("population-percent")));
-			$(".population",bar).css("right","-"+$(".population",bar).width()+"px");
-			$(".incidence .number",bar).html(formatPercent(bar.data("incidence-percent")));
-			$(".incidence",bar).css("right","-"+$(".incidence",bar).width()+"px");
+			$(".line .number",bar).html(formatPercent(bar.data("percent")));
+			$(".line .percent",bar).css("right","-"+$(".population",bar).width()+"px");
 			
-			if(comp_total.indexOf('incidence')!=-1){
-				if(!max_percent || bar.data("incidence-percent")>max_percent){
-					max_percent = bar.data("incidence-percent");
-				}
-				$(".total .percent",bar).html(formatPercent(bar.data("incidence-percent"))+"%");			
-			}else{
-				if(!max_percent || bar.data("population-percent")>max_percent){
-					max_percent = bar.data("population-percent");
-				}
-				$(".total .percent",bar).html(formatPercent(bar.data("population-percent"))+"%");
+			if(!max_percent || bar.data("percent")>max_percent){
+				max_percent = bar.data("percent");
 			}
+			$(".total .percent",bar).html(formatPercent(bar.data("percent"))+"%");
 		});
-		if(comp_total.indexOf('incidence')!=-1){
-			$(".line.population:visible").hide();
-		}else{
-			$(".line.population:hidden").show();
-		}
 		$('.bar',chart).each(function(){
 			bar = $(this);
 			container_width = $(".container",bar).width();
-			if(comp_total.indexOf('incidence')!=-1){
-				$(".line.incidence",bar).animate({
-						width:container_width*(bar.data("incidence-percent")/max_percent)
-					},{
-						duration:500
-					});
-					$(".line.population",bar).animate({
-							width:0
-						},{
-							duration:500
-						});
-			}else{
-				$(".line.population",bar).animate({
-						width:container_width*(bar.data("population-percent")/max_percent)
-					},{
-						duration:500
-					});
-					$(".line.incidence",bar).animate({
-							width:0
-						},{
-							duration:500
-						});				
-			}
-			if($(".population:visible",bar).length>0 && $(".incidence .percent",bar).width()>(container_width*(bar.data("population-percent")/max_percent))){
-				$(".incidence .percent",bar).hide();
-			}else{
-				$(".incidence .percent",bar).show();
-			}
-			$(".line .percent",bar).hide();
+			$(".line",bar).animate({
+					width:container_width*(bar.data("percent")/max_percent)
+				},{
+					duration:500
+				});
 		});
 	});
 });
--- static/js/population.js
+++ static/js/population.js
@@ -2,20 +2,8 @@ jQuery(document).ready(function(){
 	$ = jQuery;
 	setTimeout('$(".chart").trigger("load")',600);
 	
-	$("a.control").click(function(event){
-		event.preventDefault();
-		$.address.value($(this).attr('href'));
-	});
-	
-	$.address.change(function(event) {
-		if(event.value=='/'){
-			return false;
-		}
-		$("a.control").removeClass('selected');
-		$("a.control[href='"+event.value.replace("/",'')+"']").addClass("selected");
+	$(".controls input").click(function(event){
 		$(".chart").trigger("draw");
 	});
-	
-	
 });
 
