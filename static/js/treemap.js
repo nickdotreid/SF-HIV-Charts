d7b47e350b8c4657f4e4417f66f621a7bf8906b3
Nick Reid <nickreid@nickreid.com>
2011-12-01 11:19:50 -0800

    integrated jq.address

--- static/js/treemap.js
+++ static/js/treemap.js
@@ -1,77 +1,97 @@
-var w = 650,
-	h = 500,
-	color = d3.scale.category20c();
-
-$("body").delegate(".nav.axis a","click",function(event){
-	event.preventDefault();
-	data_axis = $(this).data("value");
-	
-	$(".nav.axis a.selected").removeClass("selected");
-	$(this).addClass("selected");
-	
-	$(".chart").hide();
-	if($(".chart."+data_axis).length>0){
-		$(".chart."+data_axis).show();
-		return false;
-	}
-	chart = $("#templates .chart.treemap").clone().addClass(data_axis).addClass("loading").appendTo($("#maparea")).show();
-	$(".nav ."+data_axis,chart).hide();
-	div = d3.select(".chart."+data_axis+" .map")
-		.style("position", "relative")
-		.style("width", w + "px")
-		.style("height", h + "px");
-	
-	$.ajax({
-		url:"/data/treemap.json",
-		data:{axis:data_axis},
-		dataType:"json",
-		type:"post",
-		success:function(json){
-			var treemap = d3.layout.treemap()
-				.size([w, h])
-				.sticky(true)
-				.value(function(d) { return d.size; });
-			div.data([json]).selectAll("div")
-				.data(treemap.nodes)
-			.enter().append("div")
-				.attr("class","cell")
-				.style("background",function(d){ return color(d.name);})
-				.call(cell)
-				.text(function(d){ return d.children ? null :d.name; });
-			chart.data({'treemap':treemap,'div':div}).removeClass("loading");
-			$(".nav a:first",chart).click();
-		}
-	});
-}).delegate(".nav.section a","click",function(event){
-	event.preventDefault();
-	item = $(this);
-	chart = item.parents(".chart:first");
-	$(".nav a",chart).removeClass("selected");
-	$('a.control',chart).removeClass("selected");
-	item.addClass("selected");
-	
-	div = chart.data("div");
-	treemap = chart.data("treemap");
-	
-	var total = 0;
-	div.selectAll("div")
-		.data(treemap.value(function(d){
-			val = d[item.data("value")];
-			if(!val){
-				val = 0;
-			}
-			total += val;
-			return val;
-			}))
-		.transition().duration(1500)
-		.call(cell);
-	$(".total .number",chart).html(addCommas(total));
-	
-});
-
+var color = d3.scale.category20c();
 $(document).ready(function(){
-	$(".nav a:first").click();
+	$("body").delegate(".navigation a.type","click",function(event){
+		event.preventDefault();
+		type = $(this).data("value");
+	
+		$(".navigation a.type.selected").removeClass("selected");
+		$(this).addClass("selected");
+		
+		$.address.parameter("type", type);
+		
+	//	$(".chart").trigger("draw");
+	}).delegate(".navigation a.filter","click",function(event){
+		event.preventDefault();
+		$(".navigation a.filter.selected").removeClass("selected");
+		$(this).addClass("selected");
+		$.address.parameter("filter", $(this).data("value"));
+	//	$(".chart").trigger("draw");
+	}).delegate(".chart","draw",function(){		
+		chart = $(this);
+		div = chart.data("div");
+		treemap = chart.data("treemap");
+		
+		div.selectAll("div").data(treemap.value(cell_size)).transition().duration(1500).call(cell);
+		
+	}).delegate(".chart","loadr",function(){
+		type=$(this).data("type");
+		
+		div = d3.select(".chart .map")
+			.style("position", "relative")
+			.style("width", chart.width() + "px")
+			.style("height", chart.height() + "px");
+	
+		$.ajax({
+			url:"/data/treemap.json",
+			data:{axis:type},
+			dataType:"json",
+			type:"post",
+			success:function(json){
+				$(".map div").remove();
+				var treemap = d3.layout.treemap()
+					.size([chart.width(), chart.height()])
+					.sticky(true)
+					.value(cell_size);
+				div.data([json]).selectAll("div")
+					.data(treemap.nodes)
+					.enter().append("div")
+						.attr("class","cell")
+						.style("background",function(d){ return color(d.name);})
+						.call(cell)
+						.text(function(d){ return d.children ? null :d.name; });
+				chart.data({'div':div,'treemap':treemap});
+			}
+		});
+	}).delegate(".chart","loading",function(){
+		chart = $(this);
+		$(".map div").animate({
+			opacity:0,
+		},{
+			duration:300,
+			complete:function(){
+				$(this).remove();
+			}
+		});
+	});
+	$(".navigation a.type:first").click();
 });
+
+$.address.change(function(event){
+	chart = $(".chart");
+	
+	if(chart.data("type")==$.address.parameter("type")){
+		chart.trigger("draw");
+		return true;
+	}
+	chart.data({
+		type:$.address.parameter("type"),
+		filter:$.address.parameter("filter")
+	});
+	//load delay?
+	chart.trigger("loadr");
+});
+
+function cell_size(d){
+	filter = $(".navigation a.selected.filter:first").data("value");
+	if(!filter){
+		filter = 'size';
+	}
+	val = d[filter];
+	if(!val){
+		val = 0;
+	}
+	return val;
+}
 
 function cell() {
 	this
