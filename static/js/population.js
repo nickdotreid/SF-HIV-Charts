3210315adcd28d1774d118250dfd7dcf450aef99
Nick Reid <nickreid@nickreid.com>
2011-12-06 07:28:24 -0800

    getting rid of unused files

--- static/js/population.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function(){
-	$ = jQuery;
-	setTimeout('$(".chart").trigger("load")',600);
-	
-	$(".controls input").click(function(event){
-		$(".chart").trigger("draw");
-	});
-});
-
