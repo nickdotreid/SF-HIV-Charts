34c055e7476341e1c91d5c21d79029ba97c85849
Nick Reid <nickreid@nickreid.com>
2011-12-15 15:21:54 -0800

    hard coding to make look pretty how

--- static/js/hover.js
+++ static/js/hover.js
@@ -6,6 +6,9 @@ $(document).ready(function(){
 			number.wrapInner('<div class="percent">');
 			d3.select(number[0]).append('div').attr('class','total').text(function(d){ 
 				num = d['Total'];
+				if(d['size']){
+					num = d['size']
+				}
 				return "of "+format_number(num);
 				});
 			number.append('<a href="#" class="more">More</a>');
@@ -16,12 +19,16 @@ $(document).ready(function(){
 		borderbottom = Number(number.css('border-bottom-width').replace('px',''));
 		bottom.css("top",number.height()+(bottom.height()/2)-(borderbottom/2)+'px');
 		bottom.css("left",'0px');
-		number.css("top",(0-number.height())+'px');
+		
+		//number.css("top",(0-number.height())+'px');
+		number.css("top",'-70px');
+		number.css("left",'-30px');
 	}).delegate('.hoverable','mouseleave',function(){
 		$(this).removeClass("hover");
 		number = $(".number",$(this));
 		$(".total,.more,.bottom",number).hide();
 		number.css("top",(0-number.height())+'px');
+		number.css("left",'-20px');
 	}).delegate(".number a","click",function(event){
 		event.preventDefault();
 	});
