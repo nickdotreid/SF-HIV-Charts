3b199dc7becd41f1f277ce442a17210c51a6baa4
Nick Reid <nickreid@nickreid.com>
2011-12-15 13:16:00 -0800

    added hover state

--- static/js/hover.js
+++ static/js/hover.js
@@ -1,7 +1,23 @@
 $(document).ready(function(){
 	$('body').delegate('.hoverable','mouseenter',function(){
 		$(this).addClass("hover");
+		number = $(".number",$(this));
+		if($(".percent",number).length < 1){
+			number.wrapInner('<div class="percent">');
+			d3.select(number[0]).append('div').attr('class','total').text(function(d){ 
+				num = d['Total'];
+				return "of "+format_number(num);
+				});
+			number.append('<a href="#" class="more">More</a>');
+			number.append('<div class="bottom"></div>');
+		}
+		$(".total,.more,.bottom",number).show();
+		$(".bottom",number).css("top",number.height()+6+'px');
+		number.css("top",(0-number.height())+'px');
 	}).delegate('.hoverable','mouseleave',function(){
 		$(this).removeClass("hover");
+		number = $(".number",$(this));
+		$(".total,.more,.bottom",number).hide();
+		number.css("top",(0-number.height())+'px');
 	});
 });
